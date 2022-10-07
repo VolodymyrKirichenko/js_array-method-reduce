8f8518ed6e5aeea1a9da87284d55da274421df1c
Sergio k <valodya.kirik@gmail.com>
2022-10-07 12:39:37 +0300

    changed variables names

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,19 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let previous = initialValue;
-    let start = 0;
+    let accumulator = initialValue;
+    let startIndex = 0;
 
     if (arguments.length < 2) {
-      previous = this[0];
-      start = 1;
+      accumulator = this[0];
+      startIndex = 1;
     }
 
-    for (let i = start; i < this.length; i++) {
-      previous = callback(previous, this[i], i, this);
+    for (let index = startIndex; index < this.length; index++) {
+      accumulator = callback(accumulator, this[index], index, this);
     }
 
-    return previous;
+    return accumulator;
   };
 }
 
